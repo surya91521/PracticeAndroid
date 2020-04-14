c41515b6739ce68d368ab3fd2b71079c338ee0c4
fang009 <surya91521@gmail.com>
2020-04-14 10:07:56 +0530

    work started on sqlite

--- app/src/main/java/com/example/androidpractice/Adapter.java
+++ app/src/main/java/com/example/androidpractice/Adapter.java
@@ -1,26 +1,76 @@
 package com.example.androidpractice;
 
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.squareup.picasso.Picasso;
+
 import org.xml.sax.helpers.ParserAdapter;
 
+import java.util.ArrayList;
+
 public class Adapter extends RecyclerView.Adapter<ParserAdapter.ViewHolder> {
+
+    private ArrayList<parseItem> parseItems;
+    private Context context;
+
+    public Adapter(ArrayList<parseItem> parseItems, Context context) {
+        this.parseItems = parseItems;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public Adapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.parseItem, parent, false);
+        return new RecyclerView.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ParseAdapter.ViewHolder holder, int position) {
+        ParseItem parseItem = parseItems.get(position);
+        holder.textView.setText(parseItem.getTitle());
+        Picasso.get().load(parseItem.getImgUrl()).into(holder.imageView);
+    }
+
+
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        ImageView imageView;
+        TextView textView;
+
+        public ViewHolder(@NonNull View view) {
+            super(view);
+            imageView = view.findViewById(R.id.imageView);
+            textView = view.findViewById(R.id.textView);
+            view.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            int position = getAdapterPosition();
+            ParseItem parseItem = parseItems.get(position);
+
+            Intent intent = new Intent(context, DetailActivity.class);
+            intent.putExtra("title", parseItem.getTitle());
+            intent.putExtra("image", parseItem.getImgUrl());
+            intent.putExtra("detailUrl", parseItem.getDetailUrl());
+            context.startActivity(intent);
+        }
     }
 
-    @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-
-    }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return parseItems.size();
     }
 }
