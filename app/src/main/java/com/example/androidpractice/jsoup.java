e454df8b00ccfed3fe2fc1b981bcea732724550c
fang009 <surya91521@gmail.com>
2020-04-16 23:53:10 +0530

    Jsoup image parsing work going on

--- app/src/main/java/com/example/androidpractice/jsoup.java
+++ app/src/main/java/com/example/androidpractice/jsoup.java
@@ -8,21 +8,32 @@ import android.os.Bundle;
 import android.provider.DocumentsContract;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class jsoup extends AppCompatActivity {
 
     TextView textView;
     Button button;
 
-    String url ="https://developer.android.com/";
-    String title;
+    String url ="https://www.canva.com/photos/free/";
+    String title,link,src;
     ProgressDialog progressDialog;
+
+    ImageView imageView1;
+    ImageView imageView2;
+
+    ArrayList list = new ArrayList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +51,9 @@ public class jsoup extends AppCompatActivity {
                 content.execute();
             }
         });
+
+        imageView1 = (ImageView)findViewById(R.id.imageView);
+        imageView2 = (ImageView)findViewById(R.id.imageView2);
     }
 
 
@@ -58,6 +72,10 @@ public class jsoup extends AppCompatActivity {
             super.onPostExecute(aVoid);
 
             textView.setText(title);
+            Picasso.get().load(list.get(0).toString()).into(imageView1);
+            Picasso.get().load(list.get(1).toString()).into(imageView2);
+
+
             progressDialog.cancel();
         }
 
@@ -66,7 +84,18 @@ public class jsoup extends AppCompatActivity {
 
             try {
                 Document doc = Jsoup.connect(url).get();
-                title=doc.title();
+                title=doc.title();            //gives the title of page
+              //  Elements links = doc.select("a[href]");  (Gives the title of all strings in page)
+               // link = links.text().toString();
+
+                Elements img = doc.getElementsByTag("img");
+                for(Element e1: img){
+
+                    src = e1.absUrl("src");
+                    list.add(src);
+                }
+
+
             } catch (IOException e) {
                 e.printStackTrace();
             }
