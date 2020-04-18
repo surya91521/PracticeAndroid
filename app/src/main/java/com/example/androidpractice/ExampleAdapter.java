d3b679770ea332d2945716ccb5b75c2896b4cd2e
fang009 <surya91521@gmail.com>
2020-04-18 23:17:12 +0530

    json item created and adapter going on

---
+++ app/src/main/java/com/example/androidpractice/ExampleAdapter.java
@@ -0,0 +1,7 @@
+package com.example.androidpractice;
+
+public class ExampleAdapter {
+
+
+    
+}
--- app/src/main/java/com/example/androidpractice/ExampleItem.java
+++ app/src/main/java/com/example/androidpractice/ExampleItem.java
@@ -2,5 +2,33 @@ package com.example.androidpractice;
 
 public class ExampleItem {
 
-    
+    private String mImageUrl;
+    private String mCreator;
+    private int mLikes;
+
+    public ExampleItem(String imageUrl , String creator , int likes)
+    {
+        mImageUrl=mImageUrl;
+        mCreator=creator;
+        mLikes = likes;
+    }
+
+    public String getImageUrl()
+    {
+        return  mImageUrl;
+    }
+
+    public String getCreator()
+    {
+        return mCreator;
+    }
+
+    public int getLikeCount()
+    {
+        return mLikes;
+    }
+
+
+
+
 }
